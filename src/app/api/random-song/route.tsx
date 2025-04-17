f1dac7f1cea0253cd9a8f73a3a9dc564335ca938
Fred <64082311+MatiP911@users.noreply.github.com>
2025-04-17 20:39:05 +0200

    better random (from full playlist), (modern) playlist request

--- src/app/api/random-song/route.tsx
+++ src/app/api/random-song/route.tsx
@@ -1,23 +1,33 @@
 import { NextResponse } from "next/server";
-import type { playlist } from "./interfaces.tsx"
+import type { playlist, track } from "./interfaces.tsx"
 
-export async function GET(playlistID: number) {
+export async function GET(request: Request) {
+
+    const { searchParams } = new URL(request.url);
+    const playlistID = searchParams.get("playlistID");
+
+    //TODO uncomment when its acually implemented
+    // if (!playlistID) {
+    //     return NextResponse.json({ error: "playlistID is missing or invalid" }, { status: 400 });
+    // }
+
     const response = await fetch(
         "https://api.deezer.com/playlist/9486319502"
-        // "https://api.deezer.com/playlist/" + playlistID.toString()
-        // "https://api.deezer.com/search/track?q=eminem",
+        // "https://api.deezer.com/playlist/" + playlistID
     );
+
     if (!response.ok) {
         throw new Error(`Deezer api returned error. ${response.status}`);
     }
     const fullplaylist = await response.json() as playlist;
 
-    // if (!data.data || data.data.length === 0) {
-    //     return NextResponse.json({ error: "no tracks found" }, { status: 404 });
-    // }
+    if (!fullplaylist.tracks.data || fullplaylist.tracks.data.length === 0) {
+        return NextResponse.json({ error: "no tracks found" }, { status: 404 });
+    }
 
-    const max = 10
-    const foundTrack = fullplaylist.tracks.data[Math.floor(Math.random() * max)];
+    const max = fullplaylist.tracks.data.length;
+    const foundTrack = fullplaylist.tracks.data[Math.floor(Math.random() * max)]!;
+
     const trackDetails = {
         title: foundTrack.title,
         artist: foundTrack.artist.name,
