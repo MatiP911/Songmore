84bbb543a41fc24d2e668fc6e92dec58f03ac5e8
Fred <64082311+MatiP911@users.noreply.github.com>
2025-04-17 16:16:07 +0200

    added playlist interface and moved interfaces to separate file

--- src/app/api/random-song/route.tsx
+++ src/app/api/random-song/route.tsx
@@ -1,51 +1,6 @@
 import { NextResponse } from "next/server";
+import type { DeezerSearchTrack } from "./interfaces.tsx"
 
-interface Track {
-    id: number;
-    readable?: boolean;
-    title: string;
-    title_short: string;
-    title_version: string;
-    link: string;
-    duration: number;
-    rank: number;
-    explicit_lyrics: boolean;
-    explicit_content_lyrics: number;
-    explicit_content_cover: number;
-    preview: string;
-    md5_image: string;
-    position: number;
-    artist: {
-        id: number;
-        name: string;
-        link: string;
-        picture: string;
-        picture_small: string;
-        picture_medium: string;
-        picture_big: string;
-        picture_xl: string;
-        radio: boolean;
-        tracklist: string;
-        type: string;
-    };
-    album: {
-        id: number;
-        title: string;
-        cover: string;
-        cover_small: string;
-        cover_medium: string;
-        cover_big: string;
-        cover_xl: string;
-        md5_image: string;
-        tracklist: string;
-        type: string;
-    };
-    type: string;
-}
-
-interface DeezerSeach {
-    data: Track[];
-}
 export async function GET() {
     const response = await fetch(
         "https://api.deezer.com/search/track?q=eminem",
@@ -53,7 +8,7 @@ export async function GET() {
     if (!response.ok) {
         throw new Error(`Deezer api returned error. ${response.status}`);
     }
-    const data = await response.json() as DeezerSeach;
+    const data = await response.json() as DeezerSearchTrack;
     if (!data.data || data.data.length === 0) {
         return NextResponse.json({ error: "no tracks found" }, { status: 404 });
     }
